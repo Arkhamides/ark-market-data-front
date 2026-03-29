bd05fef51afef3efe75237ff59bc465d51ba2e74
Arkhamides <joearchondis@gmail.com>
2026-03-29 20:52:26 +0200

    [gtag] updated gtag

--- src/lib/components/GoogleTag.svelte
+++ src/lib/components/GoogleTag.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
   <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-CGW03SW76E"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-YBW0PD5L00"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
